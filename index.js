52b3336e063f25e8be6840f3a96173efb9ffa0a3
rforaker <rtforaker@gmail.com>
2022-09-14 19:58:27 -0400

    Created initial HTML

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const Manager = require("./lib/Manager");
 const employees = [];
 
 function startApp() {
+    startHtml();
     addEmployee();
 }
 
@@ -75,6 +76,29 @@ function addEmployee() {
             });
             
         });
+    });
+}
+
+function startHtml() {
+    const html = `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+        <title>Team Profiles</title>
+    </head>
+    <body>
+        <nav class="navbar navbar-dark bg-success mb-5">
+            <span class="navbar-brand mb-0 h1 w-100 text-center">Team Profiles</span>
+        </nav>
+        <div class="container">
+            <div class="row">`;
+    fs.writeFile("./dist/team.html", html, function(err) {
+        if (err) {
+          console.log(err);
+        }
     });
 }
 
